f1363b07cec640af0a6a3d8581ada555378db9df
Pahul1105 <11pahulsingh@gmail.com>
2026-04-01 15:27:49 +0530

    Implement adjacency list for undirected graph
    Added adjacency list representation for an undirected graph.

--- Graphs/Adjacency List Representation.cpp
+++ Graphs/Adjacency List Representation.cpp
@@ -1,1 +1,14 @@
-
+#include<iostream>
+include namespace std;
+int main(){
+  int n,m;
+  cin>>n>>m;
+ vector<int> adjlist[n+];
+  for(int i=0;i<m;i++){
+    int u,v;
+    cin>>u>>v;
+    adjlist[u].push_back(v);
+    adjlist[v].push_back(u);  //undirected graph
+  }
+  return adjlist;
+}

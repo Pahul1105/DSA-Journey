ee0baa46106381ef01b188c89a4587d7d8be132f
Pahul1105 <11pahulsingh@gmail.com>
2026-04-01 15:32:50 +0530

    Implement adjacency list for unweighted and weighted graphs
    Added implementation for both unweighted and weighted graph representations using adjacency lists.

--- Graphs/Adjacency List Representation.cpp
+++ Graphs/Adjacency List Representation.cpp
@@ -1,3 +1,4 @@
+//UNWEIGHTED GRAPH 
 #include<iostream>
 include namespace std;
 int main(){
@@ -9,6 +10,21 @@ int main(){
     cin>>u>>v;
     adjlist[u].push_back(v);
     adjlist[v].push_back(u);  //undirected graph
+  }
+  return adjlist;
+}
+//WEIGHTED GRAPH
+#include<iostream>
+include namespace std;
+int main(){
+  int n,m;
+  cin>>n>>m;
+ vector<vector<int>> adjlist[n+];   //vector of vector
+  for(int i=0;i<m;i++){
+    int u,v,wt;
+    cin>>u>>v>>wt;
+    adjlist[u].push_back({v,wt});
+    adjlist[v].push_back({u,wt});  //undirected graph
   }
   return adjlist;
 }

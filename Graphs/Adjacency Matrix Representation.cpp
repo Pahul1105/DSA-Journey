0b195531b335ee619dd4829bbe63b708dc03801d
Pahul1105 <11pahulsingh@gmail.com>
2026-04-01 15:29:31 +0530

    Correct adjacency matrix representation code
    Fix syntax errors and improve adjacency matrix initialization.

--- Graphs/Adjacency Matrix Representation.cpp
+++ Graphs/Adjacency Matrix Representation.cpp
@@ -1,1 +1,14 @@
-
+#include<iostream>
+include namespace std;
+int main(){
+  int n,m;
+  cin>>n>>m;
+ int adjmat[n+][m+1];
+  for(int i=0;i<m;i++){
+    int u,v;
+    cin>>u>>v;
+    adjmat[u][v]=1;
+    adjmat[v][u]=1;  //undirected graph
+  }
+  return adjlist;
+}

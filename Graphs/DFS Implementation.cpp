9bfea83fc15a2d20f787f1ba4c8dfbc1f4bf75ba
Pahul1105 <11pahulsingh@gmail.com>
2026-04-03 20:48:12 +0530

    Update DFS Implementation.cpp

--- Graphs/DFS Implementation.cpp
+++ Graphs/DFS Implementation.cpp
@@ -1,1 +1,19 @@
-
+class Solution{
+private:
+void dfs(int node,vector<int> adj[], int vis[],vector<int> &ls){
+  vis[node]=1;
+  ls.push_back(node);
+  for(auto it: adj[node]){
+  if(!via[node]){
+    dfs(node,adj,vis,ls);
+  }
+  }
+}
+public:
+vector<int> dfsOfGraph(int V,vector<int> adj[]){
+  int vis[V]={0};
+  int start=0;
+  vector<int> ls;
+  dfs(start,adj,vis,ls)
+}
+};
